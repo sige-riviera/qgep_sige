e6b72021196c639340797f9ff5bcaf6c64d73366
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-04-14 10:27:08 +0200

    added script for creation, fme translation scripts

--- fme/before.sql
+++ fme/before.sql
@@ -1,4 +1,5 @@
 ALTER TABLE qgep.od_organisation  ADD COLUMN old_obj_id integer;
+ALTER TABLE qgep.od_manhole  ADD COLUMN old_obj_id integer;
 
 INSERT INTO qgep.od_organisation (old_obj_id,identifier) VALUES (10027,'ACPRS');
 INSERT INTO qgep.od_organisation (old_obj_id,identifier) VALUES (10002,'CFF');
@@ -31,11 +32,19 @@ INSERT INTO qgep.od_organisation (old_obj_id,identifier) VALUES (10009,'SIGE');
 INSERT INTO qgep.od_organisation (old_obj_id,identifier) VALUES (2,'privé');
 
 INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000013',3350,1,'circulaire');
-INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000014',3351,1,'ovoide');
-INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000015',3353,1,'rectangulaire');
-INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000016',3353,1,'galerie');
+INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000020',3351,0.75,'ovoide 0.75');
+INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000021',3351,1.5,'ovoide 1.5');
+INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000022',3351,1.20,'ovoide 1.2');
+INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000023',3353,1,'carre');
+INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000024',3353,.66,'rectangulaire 2/3');
+INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000025',3353,1.5,'rectangulaire 1.5');
+INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000026',3353,1.42,'rectangulair 850/600');
+INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000016',3353,1.32,'galerie');
 INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000017',3357,1,'inconnu');
 INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000018',5377,1,'cône asymétrique');
 INSERT INTO qgep.od_pipe_profile (obj_id,profile_type,height_width_ratio,identifier) VALUES ('ch11h8mwPP000019',5377,1,'autre');
+
+
+
  
 REFRESH MATERIALIZED VIEW qgep.vw_network_segment ;
---
+++ fme/before2.sql
@@ -0,0 +1,142 @@
+﻿BEGIN TRANSACTION;
+
+CREATE OR REPLACE VIEW qgep.vw_manhole_sige AS
+
+SELECT
+  MH.obj_id
+, MH.depth
+, MH.dimension1
+, MH.dimension2
+, MH.function
+, MH.material
+, MH.surface_inflow
+, MH.old_obj_id
+
+, WS.accessibility
+, WS.contract_section
+, WS.detail_geometry_geometry
+, WS.detail_geometry_3d_geometry
+, WS.financing, gross_costs
+, WS.identifier
+, WS.inspection_interval
+, WS.location_name
+, WS.records
+, WS.remark
+, WS.renovation_necessity
+, WS.replacement_value
+, WS.rv_base_year
+, WS.rv_construction_type
+, WS.status
+, WS.structure_condition
+, WS.subsidies
+, WS.year_of_construction
+, WS.year_of_replacement
+, WS.last_modification
+, WS.dataowner
+, WS.provider
+, WS.fk_owner
+, WS.fk_operator
+  FROM qgep.od_manhole MH
+LEFT JOIN qgep.od_wastewater_structure WS
+  ON WS.obj_id = MH.obj_id;
+
+-------------------------------------------------------
+-- MANHOLE INSERT
+-- Function: vw_manhole_insert()
+-------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION qgep.vw_manhole_insert_sige()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+  ws_obj_id character varying(16);
+BEGIN
+  INSERT INTO qgep.od_wastewater_structure (
+            obj_id
+            , accessibility
+            , contract_section
+            , detail_geometry_geometry
+            , detail_geometry_3d_geometry
+            , financing
+            , gross_costs
+            , identifier
+            , inspection_interval
+            , location_name
+            , records
+            , remark
+            , renovation_necessity
+            , replacement_value
+            , rv_base_year
+            , rv_construction_type
+            , status
+            , structure_condition
+            , subsidies
+            , year_of_construction
+            , year_of_replacement
+            , last_modification
+            , dataowner
+            , provider
+            , fk_owner
+            , fk_operator )
+    VALUES ( qgep.generate_oid('od_manhole') -- obj_id
+            , NEW.accessibility
+            , NEW.contract_section
+            , NEW.detail_geometry_geometry
+            , NEW.detail_geometry_3d_geometry
+            , NEW.financing
+            , NEW.gross_costs
+            , NEW.identifier
+            , NEW.inspection_interval
+            , NEW.location_name
+            , NEW.records
+            , NEW.remark
+            , NEW.renovation_necessity
+            , NEW.replacement_value
+            , NEW.rv_base_year
+            , NEW.rv_construction_type
+            , NEW.status
+            , NEW.structure_condition
+            , NEW.subsidies
+            , NEW.year_of_construction
+            , NEW.year_of_replacement
+            , NEW.last_modification
+            , NEW.dataowner
+            , NEW.provider
+            , NEW.fk_owner
+            , NEW.fk_operator
+           )
+           RETURNING obj_id INTO NEW.obj_id;
+
+  INSERT INTO qgep.od_manhole(
+              obj_id
+            , depth
+            , dimension1
+            , dimension2
+            , function
+            , material
+            , surface_inflow
+            , old_obj_id
+            )
+            VALUES(
+              NEW.obj_id -- obj_id
+            , NEW.depth -- depth
+            , NEW.dimension1 -- dimension1
+            , NEW.dimension2 -- dimension2
+            , NEW.function -- function
+            , NEW.material -- material
+            , NEW.surface_inflow -- surface_inflow
+            , NEW.old_obj_id
+            );
+
+  RETURN NEW;
+END; $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+DROP TRIGGER IF EXISTS vw_manhole_ON_INSERT ON qgep.vw_manhole_sige;
+
+CREATE TRIGGER vw_manhole_ON_INSERT INSTEAD OF INSERT ON qgep.vw_manhole_sige
+  FOR EACH ROW EXECUTE PROCEDURE qgep.vw_manhole_insert_sige();
+
+
+END TRANSACTION;
