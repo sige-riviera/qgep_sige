5974adc466e757d77c9322baf2458d3e26e3eea9
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-06-01 17:06:06 +0200

    started folder for direct SQL migration

---
+++ migration/backup_pg_dump.sh
@@ -0,0 +1,5 @@
+/usr/bin/pg_dump --host 172.24.171.203 --port 5432 --username "sige" --no-password  --format plain --inserts --column-inserts --verbose --file "/home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.sql" --schema "sige_assainissement" "topobase_dump"
+zip -9r /home/drouzaud/Documents/QGEP/sige/migration/dump_topobase_sql.zip /home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.sql
+rm /home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.sql
+
+/usr/bin/pg_dump --host 172.24.171.203 --port 5432 --username "sige" --no-password  --format custom --blobs --verbose --file "/home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.backup" --schema "sige_assainissement" "topobase_dump"
