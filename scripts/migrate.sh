c185163611938b8e79664a889dce9841459ad26a
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-06-02 21:48:20 +0200

    Setup OIDPREFIX

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+OIDPREFIX="ch15z36d"
+
 set -e
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.. 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 
 export PGSERVICE=pg_qgep_sige
 
-psql -c 'DROP SCHEMA IF EXISTS qgep CASCADE';
-psql -c 'DROP SCHEMA IF EXISTS sige_assainissement CASCADE';
-psql -c 'DROP SCHEMA IF EXISTS sa CASCADE';
+psql -c 'DROP SCHEMA IF EXISTS qgep CASCADE'
+psql -c 'DROP SCHEMA IF EXISTS sige_assainissement CASCADE'
+psql -c 'DROP SCHEMA IF EXISTS sa CASCADE'
 ${DIR}/QGEP-Datamodel/scripts/db_setup.sh
+
+psql -c "UPDATE qgep.is_oid_prefixes SET active=TRUE WHERE prefix='${OIDPREFIX}'"
+psql -c "UPDATE qgep.is_oid_prefixes SET active=FALSE WHERE prefix<>'${OIDPREFIX}'"
 
 pg_restore -d qgep_sige ${DIR}/migration/dump_topobase.backup
 psql -c 'ALTER SCHEMA sige_assainissement RENAME TO sa' -d qgep_sige
 
+psql -d qgep_sige -f ${DIR}/migration/mappings/function_hierarchic.sql
+
 psql -d qgep_sige -f ${DIR}/migration/cover_manhole.sql
+psql -d qgep_sige -f ${DIR}/migration/reach_channel.sql
