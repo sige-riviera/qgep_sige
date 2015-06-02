cbb620ba3a99d9bccea810e1b6476a1a92f61fb1
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-06-02 18:52:30 +0200

    More reach_channel migration code

--- migration/reach_chanel.sql
+++
@@ -1,3 +0,0 @@
-
--- compute geometry from aw_haltung_geo
-select gid, St_SetSRID(ST_GeomFromText('LINESTRING('||string_agg(y1::varchar||' '||x1::varchar, ',' order by seq)||')'),21781) as geometry from sige_assainissement.aw_haltung_geo group by gid;
---
+++ migration/reach_channel.sql
@@ -0,0 +1,25 @@
+﻿-- compute geometry from aw_haltung_geo
+WITH haltung_geo AS (SELECT gid, St_SetSRID(ST_GeomFromText('LINESTRING('||string_agg(y1::varchar||' '||x1::varchar, ',' ORDER BY seq)||')'),21781) AS geometry FROM sa.aw_haltung_geo GROUP BY gid)
+
+INSERT INTO qgep.vw_qgep_reach(
+  identifier,
+  remark,
+  length_effective, -- == haltungslaenge oder stranglaenge?
+  year_of_construction,
+  slope_per_mill, -- == gefaelle?
+  progression_geometry,
+  function_hierarchic
+)
+SELECT
+  name2,
+  bemerkung,
+  haltungslaenge,
+  baujahr,
+  gefaelle,
+  geometry,
+  fh.new
+  
+FROM sa.aw_haltung haltung
+LEFT JOIN haltung_geo geom on geom.gid = haltung.fid
+LEFT JOIN sa.map_function_hierarchic fh ON haltung.id_funktion_hierarch = fh.old
+WHERE haltung.deleted <> 1;
