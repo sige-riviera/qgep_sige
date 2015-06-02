147e353de17b007607c303bb660a049abac8e2d5
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-06-02 11:41:07 +0200

    remove plain sql file

--- migration/backup_pg_dump.sh
+++ migration/backup_pg_dump.sh
@@ -1,10 +1,10 @@
-/usr/bin/pg_dump --host 172.24.171.203 --port 5432 --username "sige" --no-password  --format plain --inserts --column-inserts --verbose --file "/home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.sql" --schema "sige_assainissement" "topobase_dump"
-zip -9r /home/drouzaud/Documents/QGEP/sige/migration/dump_topobase_sql.zip /home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.sql
-rm /home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.sql
+# /usr/bin/pg_dump --host 172.24.171.203 --port 5432 --username "sige" --no-password  --format plain --inserts --column-inserts --verbose --file "/home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.sql" --schema "sige_assainissement" "topobase_dump"
+# zip -9r /home/drouzaud/Documents/QGEP/sige/migration/dump_topobase_sql.zip /home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.sql
+# rm /home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.sql
 
 /usr/bin/pg_dump --host 172.24.171.203 --port 5432 --username "sige" --no-password  --format custom --blobs --verbose --file "/home/drouzaud/Documents/QGEP/sige/migration/dump_topobase.backup" --schema "sige_assainissement" "topobase_dump"
 
-git add dump_topobase.backup dump_topobase_sql.zip ora2pg.fmw
+git add dump_topobase.backup ora2pg.fmw
 git commit -m "updated topobase dump"
 git push
 
