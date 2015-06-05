63572d86ac0c81bcba0aed1b23faa9db3f07c838
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-06-05 12:04:37 +0200

    Remove deleted manholes from netzlinien view

--- migration/tools/od_overflow.sql
+++ migration/tools/od_overflow.sql
@@ -19,7 +19,7 @@ FROM sa.aw_netzlinie_geo
 GROUP BY gid)
 
 SELECT 
-  netzlinien.gid,
+  row_number() over() AS pk,
   aw_schacht.name2 AS ws_identifier,
   wn_from.obj_id AS wn_from_obj_id, 
   wn_to.obj_id AS wn_to_obj_id,
@@ -30,4 +30,4 @@ sa.aw_schacht
 LEFT JOIN qgep.vw_wastewater_node wn_from ON identifier = name2
 LEFT JOIN netzlinien ON from_point = wn_from.situation_geometry
 LEFT JOIN qgep.vw_wastewater_node wn_to ON to_point=wn_to.situation_geometry
-WHERE id_schachtart = 10003 OR id_schachtart = 10002 OR id_schachtart = 24
+WHERE id_schachtart = 10003 OR id_schachtart = 10002 OR id_schachtart = 24 AND COALESCE(deleted, 0) <> 0
