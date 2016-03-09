b722738f645f72fe7599e29ec38f80625ef69afb
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-03-09 10:07:33 +0100

    put things in correct order

--- migration/90_create_topology.sql
+++ migration/90_create_topology.sql
@@ -17,3 +17,10 @@ FROM qgep.od_wastewater_node no WHERE no.situation_geometry = rp.situation_geome
 
 REFRESH MATERIALIZED VIEW qgep.vw_network_segment ;
 REFRESH MATERIALIZED VIEW qgep.vw_network_node ;
+
+
+-- write manhole identifiers in reach points
+update qgep.od_reach_point rp 
+	set identifier = ws.identifier 
+	from qgep.od_wastewater_networkelement ne, qgep.od_wastewater_structure ws 
+	where rp.fk_wastewater_networkelement = ne.obj_id and ne.fk_wastewater_structure = ws.obj_id;
