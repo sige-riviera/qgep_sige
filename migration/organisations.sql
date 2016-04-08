a156a8b1fe2aecd071b473d7360ce8b2334798b2
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-04-08 15:10:35 +0200

    update project

--- migration/organisations.sql
+++ migration/organisations.sql
@@ -2,3 +2,6 @@ ALTER TABLE qgep.od_organisation  ADD COLUMN old_obj_id integer;
 
 INSERT INTO qgep.od_organisation (old_obj_id,identifier)
 SELECT id, value FROM sa.ba_eigentumsverhaeltnis_tbd;
+
+
+INSERT INTO qgep.od_organisation (identifier) VALUES ('AITV SA');
