8f2f8c7ca7c297c8b53b376f5974121d02b934a7
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-03-02 14:22:41 +0100

    option for PGSERVICE

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -5,39 +5,52 @@ set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 
-export PGSERVICE=pg_qgep
+PGSERVICE=qgep_demo
 
-psql -c 'CREATE EXTENSION IF NOT EXISTS fineltra'
-psql -c 'DROP SCHEMA IF EXISTS qgep CASCADE'
-psql -c 'DROP SCHEMA IF EXISTS sige_assainissement CASCADE'
-psql -c 'DROP SCHEMA IF EXISTS sa CASCADE'
-${DIR}/datamodel/scripts/db_setup.sh -s 2056
+while getopts ":p:" opt; do
+  case $opt in
+    s)
+      PGSERVICE=$OPTARG
+      echo "-p was triggered, PGSERVICE: $PGSERVICE" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
 
-psql -c "UPDATE qgep.is_oid_prefixes SET active=TRUE WHERE prefix='${OIDPREFIX}'"
-psql -c "UPDATE qgep.is_oid_prefixes SET active=FALSE WHERE prefix<>'${OIDPREFIX}'"
 
-pg_restore --no-owner -d qgep_sige ${DIR}/migration/dump_topobase_95.backup
-psql -c 'ALTER SCHEMA sige_assainissement RENAME TO sa'
+psql "service=${PGSERVICE}" -c 'CREATE EXTENSION IF NOT EXISTS fineltra'
+psql "service=${PGSERVICE}" -c 'DROP SCHEMA IF EXISTS qgep CASCADE'
+psql "service=${PGSERVICE}" -c 'DROP SCHEMA IF EXISTS sige_assainissement CASCADE'
+psql "service=${PGSERVICE}" -c 'DROP SCHEMA IF EXISTS sa CASCADE'
+${DIR}/datamodel/scripts/db_setup.sh -s 2056 -p $PGSERVICE
 
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/aggregates_first_last.sql
+psql "service=${PGSERVICE}" -c "UPDATE qgep.is_oid_prefixes SET active=TRUE WHERE prefix='${OIDPREFIX}'"
+psql "service=${PGSERVICE}" -c "UPDATE qgep.is_oid_prefixes SET active=FALSE WHERE prefix<>'${OIDPREFIX}'"
 
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/function_hierarchic.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/reach_horizontal_positioning.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/manhole_function.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/usage_current.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/status.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/reach_material.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/elevation_determination.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/function_hydraulic.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/access_aid_kind.sql
+PGSERVICE=${PGSERVICE} pg_restore --no-owner -d ${PGSERVICE} ${DIR}/migration/dump_topobase_95.backup
+psql "service=${PGSERVICE}" -c 'ALTER SCHEMA sige_assainissement RENAME TO sa'
 
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/organisations.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/cover_manhole.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/profiles.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/reach_channel.sql
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/prank_weir.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/aggregates_first_last.sql
 
-psql -v ON_ERROR_STOP=1  -f ${DIR}/datamodel/07_views_for_network_tracking.sql # not sure why we need to rerun this one
-psql -v ON_ERROR_STOP=on -f ${DIR}/migration/90_create_topology.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/function_hierarchic.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/reach_horizontal_positioning.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/manhole_function.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/usage_current.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/status.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/reach_material.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/elevation_determination.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/function_hydraulic.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/access_aid_kind.sql
 
-OWNER=qgep SCHEMA=qgep DATABASE=qgep_sige ${DIR}/datamodel/scripts/change_owner.sh
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/organisations.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/cover_manhole.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/profiles.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/reach_channel.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/prank_weir.sql
+
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1  -f ${DIR}/datamodel/07_views_for_network_tracking.sql # not sure why we need to rerun this one
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/90_create_topology.sql
+
+OWNER=qgep SCHEMA=qgep DATABASE=${PGSERVICE} ${DIR}/datamodel/scripts/change_owner.sh
