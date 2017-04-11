cbbe488405aae5dd53f2426ddfd720b1fdef527c
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-04-11 10:13:12 +0200

    manhole label, inspection form

--- translate_project/translate.sh
+++ translate_project/translate.sh
@@ -1,2 +1,4 @@
 /home/drouzaud/opt/QGIS2/build/output/bin/qgis /home/drouzaud/Documents/QGEP/sige/translate_project/translate.qgs
 sed -i -r 's/^(\s*<attributeEditorRelation .*?)relation=""(.*)name="(.*?)"(.*?)$/\1 relation="\3"\2name="\3"\4/g' /home/drouzaud/Documents/QGEP/sige/qgis-project/qgep_sige_auto.qgs
+/home/drouzaud/opt/QGIS2/build/output/bin/qgis /home/drouzaud/Documents/QGEP/sige/qgis-project/qgep_sige_auto.qgs &
+
