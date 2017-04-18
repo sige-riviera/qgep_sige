d7ce11e0afdae02731aed26fbadeaca27ffc9784
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2017-04-18 10:52:36 +0200

    Modifications pour Pully du fichier migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-OIDPREFIX="ch15z36d"
+# Préfix to be ordered here : http://www.interlis.ch/oid/oid_commande_f.php
+OIDPREFIX="ch176dc9"
 
 set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 
-PGSERVICE=qgep_demo
+# Service to be defined in PG_SERVICE.conf file
+PGSERVICE=pg_qgep
 
 while getopts ":p:" opt; do
   case $opt in
@@ -19,20 +21,27 @@ while getopts ":p:" opt; do
   esac
 done
 
-psql "service=${PGSERVICE}" -c 'CREATE EXTENSION IF NOT EXISTS fineltra'
+# Reframe option to get to MN95 srs. ! Does not work on windows yet!
+#psql "service=${PGSERVICE}" -c 'CREATE EXTENSION IF NOT EXISTS fineltra'
 psql "service=${PGSERVICE}" -c 'DROP SCHEMA IF EXISTS qgep CASCADE'
-psql "service=${PGSERVICE}" -c 'DROP SCHEMA IF EXISTS sige_assainissement CASCADE'
-psql "service=${PGSERVICE}" -c 'DROP SCHEMA IF EXISTS sa CASCADE'
-${DIR}/datamodel/scripts/db_setup.sh -s 2056 -p $PGSERVICE
+#psql "service=${PGSERVICE}" -c 'DROP SCHEMA IF EXISTS sige_assainissement CASCADE'
+#psql "service=${PGSERVICE}" -c 'DROP SCHEMA IF EXISTS sa CASCADE'
 
+# Initialize datamodel
+${DIR}/datamodel/scripts/db_setup.sh -p $PGSERVICE
+
+# Organisation prefix activation
 psql "service=${PGSERVICE}" -c "UPDATE qgep.is_oid_prefixes SET active=TRUE WHERE prefix='${OIDPREFIX}'"
 psql "service=${PGSERVICE}" -c "UPDATE qgep.is_oid_prefixes SET active=FALSE WHERE prefix<>'${OIDPREFIX}'"
 
-PGSERVICE=${PGSERVICE} pg_restore --no-owner -d ${PGSERVICE} ${DIR}/migration/dump_topobase_95.backup
-psql "service=${PGSERVICE}" -c 'ALTER SCHEMA sige_assainissement RENAME TO sa'
+# Option to restore from backup.
+#PGSERVICE=${PGSERVICE} pg_restore --no-owner -d ${PGSERVICE} ${DIR}/migration/dump_topobase_95.backup
+#psql "service=${PGSERVICE}" -c 'ALTER SCHEMA sige_assainissement RENAME TO sa'
 
+# Function first last
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/aggregates_first_last.sql
 
+# Mappings
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/function_hierarchic.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/reach_horizontal_positioning.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/manhole_function.sql
@@ -43,13 +52,16 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/ele
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/function_hydraulic.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/mappings/access_aid_kind.sql
 
+# Migration
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/organisations.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/cover_manhole.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/profiles.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/reach_channel.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/prank_weir.sql
 
+# Recreate views
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1  -f ${DIR}/datamodel/07_views_for_network_tracking.sql # not sure why we need to rerun this one
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/90_create_topology.sql
 
+# Change owner
 PGSERVICE=${PGSERVICE} OWNER=qgep SCHEMA=qgep DATABASE=${PGSERVICE} ${DIR}/datamodel/scripts/change_owner.sh
