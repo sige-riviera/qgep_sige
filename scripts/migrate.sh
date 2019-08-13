f2f20df98326665e87bc6371d2282ab75d73e36b
Arnaud Poncet Montanges <arnaudponcetmontanges@gmail.com>
2019-08-13 13:14:01 +0200

    Ajout de la création des vues QGEP

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -83,3 +83,8 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=on -f ${DIR}/migration/prank_weir.s
 
 # Change owner
 PGSERVICE=${PGSERVICE} OWNER=qgep SCHEMA=qgep_od DATABASE=qgep_poc ${DIR}/datamodel/scripts/change_owner.sh
+
+# Add Export views
+
+${DIR}/datamodel/view/export/insert_export_views.sh
+
