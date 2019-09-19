fba82b99032c49f69f2a6b4f888c7429cbb815c5
ponceta <arnaudponcetmontanges@gmail.com>
2019-09-19 07:15:11 -0700

    Fix wrong topology file

--- migration/topologie.sql
+++ migration/topologie.sql
@@ -1,4 +1,10 @@
-INSERT INTO qgep_od.data_media (obj_id, identifier, kind, location, path, remark, last_modification, fk_dataowner, fk_provider) VALUES
-('ch176dc9VO000000', 'Photos des chambres d''assainissement', 3789, 'G SIGIP Raster', 'G:/SIGIP/Raster/Photos/Chambres_assainissement/','\N', '2019-08-14 11:12:04.213293', 1, 1),
-('ch176dc9VO000001', 'Vidéos des collecteurs', 3789, 'G Vidéos collecteurs', 'G:/Vidéos Collecteurs/', '\N', '2019-08-14 11:11:54.903049', 1, 1)
-;
+UPDATE qgep_od.reach_point
++SET fk_wastewater_networkelement = selection.obj_id
++FROM
++(
++SELECT obj_id,identifier
++
++FROM qgep_od.wastewater_networkelement
++
++WHERE obj_id LIKE '%WE%') AS selection
++WHERE reach_point.identifier IS NOT NULL AND reach_point.identifier = selection.identifier;
