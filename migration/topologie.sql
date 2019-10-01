2c33742e8da70e0b2c961f608f3c151914a1a50f
ponceta <arnaudponcetmontanges@gmail.com>
2019-10-01 05:54:22 -0700

    Fix topologie

--- migration/topologie.sql
+++ migration/topologie.sql
@@ -1,10 +1,10 @@
 UPDATE qgep_od.reach_point
-+SET fk_wastewater_networkelement = selection.obj_id
-+FROM
-+(
-+SELECT obj_id,identifier
-+
-+FROM qgep_od.wastewater_networkelement
-+
-+WHERE obj_id LIKE '%WE%') AS selection
-+WHERE reach_point.identifier IS NOT NULL AND reach_point.identifier = selection.identifier;
+SET fk_wastewater_networkelement = selection.obj_id
+FROM
+(
+SELECT obj_id,identifier
+
+FROM qgep_od.wastewater_networkelement
+
+WHERE obj_id LIKE '%WN%') AS selection
+WHERE reach_point.identifier IS NOT NULL AND reach_point.identifier = selection.identifier;
