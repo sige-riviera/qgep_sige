0e8d2406e6fd91d252e0a6c186baf0838b62658e
ponceta <arnaudponcetmontanges@gmail.com>
2019-11-07 07:29:12 -0800

    Ajout des détails d'ouvrages spéciaux

--- migration/detail_ouvrages.sql
+++ migration/detail_ouvrages.sql
@@ -1,3 +1,5 @@
+--Update detail wastewater structure
+
 UPDATE qgep_od.wastewater_structure
 
 SET detail_geometry_geometry = ST_Force3D(ST_Multi(selection.geom))
@@ -22,7 +24,6 @@ SELECT
 FROM pully_ass_spatial.aw_schachtdetail
 WHERE   ST_GeometryType(geom) = 'ST_Polygon'
 	AND fid_schacht IS NOT NULL
-	
 UNION
 SELECT
   fid,
@@ -43,3 +44,32 @@ FROM belmont_ass_spatial.aw_schachtdetail
 WHERE   ST_GeometryType(geom) = 'ST_Polygon'
 	AND fid_schacht IS NOT NULL) AS selection
 WHERE wastewater_structure.pully_id_topobase = selection.fid_schacht::character varying;
+
+--Update detailed wastwater structures
+
+UPDATE qgep_od.wastewater_structure
+SET detail_geometry_geometry = ST_Force3D(ST_Multi(selection.geom))
+FROM(
+SELECT geom,schacht.fid as fid_schacht
+FROM pully_ass.aw_schacht schacht
+LEFT JOIN pully_ass.aw_sonderbauwerk sbw ON sbw.fid_schacht = schacht.fid
+LEFT JOIN pully_ass_spatial.aw_sbwdetail detail ON detail.fid_sonderbauwerk = sbw.fid
+WHERE sbw.fid_schacht IS NOT NULL
+AND detail.fid_sonderbauwerk IS NOT NULL
+AND ST_GeometryType(geom) = 'ST_Polygon') as selection
+WHERE selection.fid_schacht::character varying = wastewater_structure.pully_id_topobase;
+
+--Update sbw detail
+
+UPDATE qgep_od.wastewater_structure
+SET detail_geometry_geometry = ST_Force3D(ST_Multi(selection.geom))
+FROM(
+SELECT geom,schacht.fid as fid_schacht
+FROM pully_ass.aw_schacht schacht
+LEFT JOIN pully_ass.aw_sbw_wehr sbw_w ON sbw_w.s_id = schacht.fid
+LEFT JOIN pully_ass_spatial.aw_sbwdetail detail ON detail.fid_sbw_wehr = sbw_w.fid
+WHERE sbw_w.s_id IS NOT NULL
+AND detail.fid_sbw_wehr IS NOT NULL
+AND ST_GeometryType(geom) = 'ST_Polygon') as selection
+WHERE selection.fid_schacht::character varying = wastewater_structure.pully_id_topobase;
+
