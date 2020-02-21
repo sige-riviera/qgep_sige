796814294f477dd0c1b07941a9e232a910eabfd6
Arnaud Poncet-Montanges <ponceta@users.noreply.github.com>
2020-02-21 14:55:00 +0100

    Fix id -> obj_id to enable replay function
    Fix replay function event

--- migration/audit.sql
+++ migration/audit.sql
@@ -1,4 +1,4 @@
-SELECT qgep_sys.audit_view('qgep_od.vw_qgep_wastewater_structure', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-SELECT qgep_sys.audit_view('qgep_od.vw_qgep_reach', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-SELECT qgep_sys.audit_view('qgep_od.vw_cover', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-SELECT qgep_sys.audit_view('qgep_od.vw_wastewater_node', 'true'::boolean, '{}'::text[], '{id}'::text[]);
+SELECT qgep_sys.audit_view('qgep_od.vw_qgep_wastewater_structure', 'true'::boolean, '{}'::text[], '{obj_id}'::text[]);
+SELECT qgep_sys.audit_view('qgep_od.vw_qgep_reach', 'true'::boolean, '{}'::text[], '{obj_id}'::text[]);
+SELECT qgep_sys.audit_view('qgep_od.vw_cover', 'true'::boolean, '{}'::text[], '{obj_id}'::text[]);
+SELECT qgep_sys.audit_view('qgep_od.vw_wastewater_node', 'true'::boolean, '{}'::text[], '{obj_id}'::text[]);
